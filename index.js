0b450f9c83f61d3c11212511fdc6c49570066649
riteshjha61253 <jha61253@gmail.com>
2025-08-19 12:37:52 +0530

    Mounted Routes in Index file

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const connection = require('./config/db');
+const schoolRoutes = require('./routes/schoolRoutes');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('/', schoolRoutes); 
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
