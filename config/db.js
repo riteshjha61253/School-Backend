d28bb5e1325e964da1a9cec90e4d8e240792e4c4
riteshjha61253 <jha61253@gmail.com>
2025-08-20 11:54:35 +0530

    Deployment changes

--- config/db.js
+++ config/db.js
@@ -2,9 +2,10 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+  database: process.env.DB_NAME,
 });
 
 connection.connect(err => {
